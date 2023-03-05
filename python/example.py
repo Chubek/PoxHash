ffe8085f3b72bcb3d403f66fdce11188714274b6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-05 17:25:43 +0330

    removed semicolon

--- python/example.py
+++ python/example.py
@@ -2,7 +2,7 @@ from libpox import pox_hash
 import sys
 
 if __name__ == "__main__":
-    print("Pox (Python) hashes for passed strings in hexdigest form:");
+    print("Pox (Python) hashes for passed strings in hexdigest form:")
     for i, arg in enumerate(sys.argv[1:]):
         hash = pox_hash(arg.encode())
         hex = hash.hexdigest
