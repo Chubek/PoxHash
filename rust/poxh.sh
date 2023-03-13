bb90f8b032f27a578619a8592d396f27c045a594
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-13 15:03:42 +0330

    added opt flag

--- rust/poxh.sh
+++ rust/poxh.sh
@@ -1,4 +1,4 @@
 rm -f /tmp/rust-poxhash-runner /tmp/libpoxrust.rlib
-rustc rust/libpoxh.rs --crate-type lib --crate-name libpoxh -o /tmp/libpoxrust.rlib
-rustc rust/runner.rs --extern libpoxh=/tmp/libpoxrust.rlib -o /tmp/rust-poxhash-runner
+rustc rust/libpoxh.rs -O --crate-type lib --crate-name libpoxh -o /tmp/libpoxrust.rlib
+rustc rust/runner.rs -O --extern libpoxh=/tmp/libpoxrust.rlib -o /tmp/rust-poxhash-runner
 /tmp/rust-poxhash-runner $@
