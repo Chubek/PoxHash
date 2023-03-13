a149ac37b90fdfd6052e45bb1aad85cdd6ebd5ad
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-13 14:30:23 +0330

    added release flag

--- go/poxh.sh
+++ go/poxh.sh
@@ -3,5 +3,5 @@ cp go/libpoxh.go /tmp/poxgo/libpoxh/libpoxh.go
 cp go/runner.go /tmp/poxgo/main.go
 rm -f /tmp/poxgo/go-poxhash-runner
 rm -f /tmp/poxgo/go.mod
-cd /tmp/poxgo  && go mod init pox &> /dev/null && go mod tidy && go build -o /tmp/poxgo/go-poxhash-runner main.go 
+cd /tmp/poxgo  && go mod init pox &> /dev/null && go mod tidy && go build -ldflags "-s -w" -o /tmp/poxgo/go-poxhash-runner main.go 
 /tmp/poxgo/go-poxhash-runner $@
