8907208e380f2c508260a7882e626664e4c679f6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-16 01:49:28 +0330

    updated licnese info and added integirty hashg to all libpoxh.* files

--- js/libpoxh.js
+++ js/libpoxh.js
@@ -1,9 +1,31 @@
-//////////////////////////////////////////////
-//      PoxHash v1                          //
-//      Implementation in JavaScript			  //
-//      By Chubak Bidpaa - March 2023       //
-//      License: GPLv3                      //
-//////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+//                     PoxHash implementation in JavaScript                        //
+//                          March 2023 - Chubak Bidpaa                             //
+//                 Integrity: `CoffeeCup` -> 2764F3877155A9D7                      //
+/////////////////////////////////////////////////////////////////////////////////////
+// MIT License                                                                     //
+//                                                                                 //
+// Copyright (c) 2023 Chubak Bidpaa                                                //
+//                                                                                 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy    //
+// of this software and associated documentation files (the "Software"), to deal   //
+// in the Software without restriction, including without limitation the rights    //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       //
+// copies of the Software, and to permit persons to whom the Software is           //
+// furnished to do so, subject to the following conditions:                        //
+//                                                                                 //
+// The above copyright notice and this permission notice shall be included in all  //
+// copies or substantial portions of the Software.                                 //
+//                                                                                 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     //
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   //
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   //
+// SOFTWARE.                                                                       //
+/////////////////////////////////////////////////////////////////////////////////////
+
 
 const cPOX_ROUND_PRIMES = new Uint16Array([
   0x0377, 0x0683, 0x05fb, 0x05fb, 0x0665, 0x074b, 0x036d, 0x033d, 0x0115,
