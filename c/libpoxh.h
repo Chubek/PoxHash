83879ff6d3e1950a4c44f554c40c965d307df751
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-16 01:50:48 +0330

    updated licnese info and added integirty hashg to all libpoxh.* files

--- c/libpoxh.h
+++ c/libpoxh.h
@@ -1,9 +1,30 @@
-//////////////////////////////////////////////
-//      PoxHash v1                          //
-//      Implementation in Header-Only C     //
-//      By Chubak Bidpaa - March 2023       //
-//      License: GPLv3                      //
-//////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+//                       PoxHash implementation in C                               //
+//                          March 2023 - Chubak Bidpaa                             //
+//                 Integrity: `Antimagnetic` -> DE136D78AF59E773                   //
+/////////////////////////////////////////////////////////////////////////////////////
+// MIT License                                                                     //
+//                                                                                 //
+// Copyright (c) 2023 Chubak Bidpaa                                                //
+//                                                                                 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy    //
+// of this software and associated documentation files (the "Software"), to deal   //
+// in the Software without restriction, including without limitation the rights    //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       //
+// copies of the Software, and to permit persons to whom the Software is           //
+// furnished to do so, subject to the following conditions:                        //
+//                                                                                 //
+// The above copyright notice and this permission notice shall be included in all  //
+// copies or substantial portions of the Software.                                 //
+//                                                                                 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     //
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   //
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   //
+// SOFTWARE.                                                                       //
+/////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef __POX_H
 #define __POX_H
@@ -786,7 +807,7 @@ typedef struct PoxDigest
  */
 extern inline poxhash_t pox_hash(uint8_t *message)
 {
-    size_t length_message = strlen(message);
+    size_t length_message = strlen((const char *)message);
 
     uint8_t block_array[POX_BLOCK_NUM] = {0};
     uint8_t portion_array[POX_PORTION_NUM] = {0};
--- rust/libpoxh.rs
+++ rust/libpoxh.rs
@@ -1,9 +1,30 @@
-//////////////////////////////////////////////
-//      PoxHash v1                          //
-//      Implementation in Rust 			    //
-//      By Chubak Bidpaa - March 2023       //
-//      License: GPLv3                      //
-//////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+//                       PoxHash implementation in Rust                            //
+//                          March 2023 - Chubak Bidpaa                             //
+//                 Integrity: `Westoxificated` -> C4DCCB62B2C2D8A4                 //
+/////////////////////////////////////////////////////////////////////////////////////
+// MIT License                                                                     //
+//                                                                                 //
+// Copyright (c) 2023 Chubak Bidpaa                                                //
+//                                                                                 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy    //
+// of this software and associated documentation files (the "Software"), to deal   //
+// in the Software without restriction, including without limitation the rights    //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       //
+// copies of the Software, and to permit persons to whom the Software is           //
+// furnished to do so, subject to the following conditions:                        //
+//                                                                                 //
+// The above copyright notice and this permission notice shall be included in all  //
+// copies or substantial portions of the Software.                                 //
+//                                                                                 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     //
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   //
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   //
+// SOFTWARE.                                                                       //
+/////////////////////////////////////////////////////////////////////////////////////
 
 mod consts {
 
