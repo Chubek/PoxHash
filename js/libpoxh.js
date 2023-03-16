a4c41d4ce13cd42b69dabffc699bdd8a5d01b4a7
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-16 14:33:01 +0330

    refactored js

--- js/libpoxh.js
+++ js/libpoxh.js
@@ -26,7 +26,53 @@
 // SOFTWARE.                                                                       //
 /////////////////////////////////////////////////////////////////////////////////////
 
+// CONSTANTS
 
+/// INITIAL PRIME NUMBERS
+/// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#initial-prime-numbers
+const cPOX_PRIME_INIT_A = 0x17cb;
+const cPOX_PRIME_INIT_B = 0x0371;
+const cPOX_PRIME_INIT_C = 0x2419;
+const cPOX_PRIME_INIT_D = 0xf223;
+
+/// SIZE CONSTANTS
+/// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#size-constants
+const cPOX_ROUND_PRIME_NUM = 90;
+const cPOX_BLOCK_NUM = 64;
+const cPOX_8B_PRIME_NUM = 54;
+const cPOX_ROUND_NUM = 31;
+const cPOX_CHUNK_NUM = 16;
+const cPOX_PORTION_NUM = 4;
+const cPOX_SD_PRIME_NUM = 3;
+const cPOX_MAGIC_PRIME_NUM = 2;
+
+/// BIT-RELATED CONSTANTS
+/// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#bit-related-constants
+const cWORD_WIDTH = 16;
+const cBYTE_WIDTH = 8;
+const cUINT16_MAX = 2 ** 16 - 1;
+
+/// MASKS
+/// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#masks
+const cMASK_DWORD_4F4Z = 0xffff0000;
+const cMASK_DWORD_4Z4F = 0x0000ffff;
+const cMASK_WORD_FZFZ = 0xf0f0;
+const cMASK_WORD_ZFZF = 0x0f0f;
+const cMASK_WORD_FZZZ = 0xf000;
+const cMASK_WORD_ZZFZ = 0x00f0;
+const cMASK_WORD_ZZZF = 0x000f;
+const cMASK_WORD_ZZFF = 0x00ff;
+const cMASK_WORD_FFZZ = 0xff00;
+const cMASK_WORD_FZZF = 0xf00f;
+const cMASK_WORD_FFFZ = 0xfff0;
+const cMASK_WORD_ZFFF = 0x0fff;
+const cMASK_NIBBLET_01 = 0b01;
+const cMASK_NIBBLET_10 = 0b10;
+const cMASK_NIBBLET_11 = 0b11;
+const cMASK_NIBBLET_00 = 0b00;
+
+/// PRIME_ARRAYS
+/// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#prime-arrays
 const cPOX_ROUND_PRIMES = new Uint16Array([
   0x0377, 0x0683, 0x05fb, 0x05fb, 0x0665, 0x074b, 0x036d, 0x033d, 0x0115,
   0x07cf, 0x0e59, 0x0e75, 0x0a75, 0x119b, 0x1073, 0x12b3, 0x0fd1, 0x0a75,
@@ -49,41 +95,8 @@ const cPOX_8B_PRIMES = new Uint16Array([
 const cPOX_SINGLE_DIGIT_PRIMES = new Uint16Array([0x3, 0x5, 0x7]);
 const cPOX_MAGIC_PRIMES = new Uint16Array([0x33, 0x65]);
 
-const cPOX_PRIME_INIT_A = 0x17cb;
-const cPOX_PRIME_INIT_B = 0x0371;
-const cPOX_PRIME_INIT_C = 0x2419;
-const cPOX_PRIME_INIT_D = 0xf223;
-
-const cPOX_ROUND_PRIME_NUM = 90;
-const cPOX_BLOCK_NUM = 64;
-const cPOX_8B_PRIME_NUM = 54;
-const cPOX_ROUND_NUM = 31;
-const cPOX_CHUNK_NUM = 16;
-const cPOX_PORTION_NUM = 4;
-const cPOX_SD_PRIME_NUM = 3;
-const cPOX_MAGIC_PRIME_NUM = 2;
-
-const cWORD_WIDTH = 16;
-const cBYTE_WIDTH = 8;
-const cUINT16_MAX = 2 ** 16 - 1;
-
-const cMASK_DWORD_4F4Z = 0xffff0000;
-const cMASK_DWORD_4Z4F = 0x0000ffff;
-const cMASK_WORD_FZFZ = 0xf0f0;
-const cMASK_WORD_ZFZF = 0x0f0f;
-const cMASK_WORD_FZZZ = 0xf000;
-const cMASK_WORD_ZZFZ = 0x00f0;
-const cMASK_WORD_ZZZF = 0x000f;
-const cMASK_WORD_ZZFF = 0x00ff;
-const cMASK_WORD_FFZZ = 0xff00;
-const cMASK_WORD_FZZF = 0xf00f;
-const cMASK_WORD_FFFZ = 0xfff0;
-const cMASK_WORD_ZFFF = 0x0fff;
-const cMASK_NIBBLET_01 = 0b01;
-const cMASK_NIBBLET_10 = 0b10;
-const cMASK_NIBBLET_11 = 0b11;
-const cMASK_NIBBLET_00 = 0b00;
-
+/// MISC
+/// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#misc
 const cCOMB_BIONOM = [
   [0, 1],
   [0, 2],
@@ -92,6 +105,9 @@ const cCOMB_BIONOM = [
   [1, 3],
   [2, 3],
 ];
+const cBIONOM_SIZE = 6;
+
+/// CONVERSION CONSTANTS
 const cSEX_CHARS = [
   "0",
   "1",
@@ -231,11 +247,183 @@ const cOCT_BASE = 8;
 const cSEN_BASE = 6;
 const cBIN_BASE = 2;
 
-const cBIONOM_SIZE = 6;
+////////    TOOLS     //////////
 
 const iDiv = (a, b) => {
   return ~~(a / b);
 };
+
+const maxAndArgMax = (ls) => {
+  let currMax = ls[0];
+  let currIndex = 0;
+
+  for (let i = 1; i < cPOX_PORTION_NUM; i++) {
+    if (ls[i] > currMax) {
+      currMax = ls[i];
+      currIndex = i;
+    }
+  }
+
+  return { max: currMax, argmax: currIndex };
+};
+
+const minAndArgMin = (ls) => {
+  let currMin = ls[0];
+  let currIndex = 0;
+
+  for (let i = 1; i < cPOX_PORTION_NUM; i++) {
+    if (ls[i] < currMin) {
+      currMin = ls[i];
+      currIndex = i;
+    }
+  }
+
+  return { min: currMin, argmin: currIndex };
+};
+
+const log2N = (num) => {
+  return num > 1 ? 1 + log2N(iDiv(num, 2)) : 0;
+};
+
+////////  .TOOLS      ////////
+//-------------------------//
+//////// CONVERSION ////////
+// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#part-g-conversion--preparation-prep-methods
+
+const wordArrayToByteArray = (wordarray) => {
+  const wordToBye = (word) => {
+    const lower = word & cMASK_WORD_ZZFF;
+    const upper = (word & cMASK_WORD_FFZZ) >> cBYTE_WIDTH;
+
+    return { lower: lower, upper: upper };
+  };
+
+  const upperLowerA = wordToBye(wordarray[0]);
+  const upperLowerB = wordToBye(wordarray[1]);
+  const upperLowerC = wordToBye(wordarray[2]);
+  const upperLowerD = wordToBye(wordarray[3]);
+
+  const byteArray = new Uint8Array([
+    upperLowerA.lower,
+    upperLowerA.upper,
+    upperLowerB.lower,
+    upperLowerB.upper,
+    upperLowerC.lower,
+    upperLowerC.upper,
+    upperLowerD.lower,
+    upperLowerD.upper,
+  ]);
+
+  return byteArray;
+};
+
+const wordToDouble = (wordA, wordB, dArr, index) => {
+  const wArrD = new Uint32Array([wordA, wordB]);
+  dArr[index] |= wArrD[0];
+  dArr[index] |= wArrD[1] << 16;
+};
+
+const wordArrayToDoubleArray = (wArray) => {
+  let dArr = new Uint32Array(2);
+  wordToDouble(wArray[0], wArray[1], dArr, 0);
+  wordToDouble(wArray[2], wArray[3], dArr, 1);
+  return dArr;
+};
+
+const doubleArrayToQuad = (dArr) => {
+  let quad = BigInt("0x0000000000000000");
+  quad |= BigInt(dArr[0]);
+  quad |= BigInt(dArr[1]) << BigInt(32);
+  return BigInt.asUintN(64, quad);
+};
+
+const byteArrayToWordArrayAndPad = (bytearr) => {
+  let size = bytearr.length;
+  while (size % cPOX_BLOCK_NUM != 0) {
+    size += 1;
+  }
+  let u16Array = new Uint16Array(size);
+  for (let i = 0; i < size; i++) {
+    u16Array[i] = bytearr[i];
+  }
+  return u16Array;
+};
+
+const convertBaseFromDecimal = (base, size, chars, res, dec, offset) => {
+  for (let i = size * offset + size - 1; i >= size * offset; i--) {
+    res[i] = chars[dec % base];
+    dec = iDiv(dec, base);
+  }
+};
+
+const convertWordsToSexDigest = (words) => {
+  let sex = "0".repeat(cSEX_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cSEX_BASE, cSEX_SIZE, cSEX_CHARS, sex, words[i], i);
+  }
+  return sex.join("");
+};
+
+const convertWordsToVigDigest = (words) => {
+  let vig = "0".repeat(cVIG_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cVIG_BASE, cVIG_SIZE, cVIG_CHARS, vig, words[i], i);
+  }
+  return vig.join("");
+};
+
+const convertWordsToHexDigest = (words) => {
+  let hex = "0".repeat(cHEX_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cHEX_BASE, cHEX_SIZE, cHEX_CHARS, hex, words[i], i);
+  }
+  return hex.join("");
+};
+
+const convertWordsToTetDigest = (words) => {
+  let tet = "0".repeat(cTET_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cTET_BASE, cTET_SIZE, cTET_CHARS, tet, words[i], i);
+  }
+  return tet.join("");
+};
+
+const convertWordsToDuoDigest = (words) => {
+  let duo = "0".repeat(cDUO_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cDUO_BASE, cDUO_SIZE, cDUO_CHARS, duo, words[i], i);
+  }
+  return duo.join("");
+};
+
+const convertWordsToOctDigest = (words) => {
+  let oct = "0".repeat(cOCT_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cOCT_BASE, cOCT_SIZE, cOCT_CHARS, oct, words[i], i);
+  }
+  return oct.join("");
+};
+
+const convertWordsToSenDigest = (words) => {
+  let sen = "0".repeat(cSEN_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cSEN_BASE, cSEN_SIZE, cSEN_CHARS, sen, words[i], i);
+  }
+  return sen.join("");
+};
+
+const convertWordsToBinDigest = (words) => {
+  let bin = "0".repeat(cBIN_SIZE * cPOX_PORTION_NUM).split("");
+  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
+    convertBaseFromDecimal(cBIN_BASE, cBIN_SIZE, cBIN_CHARS, bin, words[i], i);
+  }
+  return bin.join("");
+};
+
+//////// .CONVERSION ////////
+//-------------------------//
+//////// BITWISE OPS ////////
+// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#part-b-bitwise-operations
 
 const omega = (dwArr) => {
   dwArr[0] = (dwArr[0] & cMASK_DWORD_4F4Z) >> cWORD_WIDTH;
@@ -248,6 +436,10 @@ const epsilon = (dwArr) => {
 const lamed = (dwArr, by) => {
   dwArr[0] = (dwArr[0] << by) | (dwArr[0] >> (cWORD_WIDTH - by));
 };
+
+//////// .BITWISE OPS ////////
+//--------------------------//
+//////// BESPOKE OPS ////////
 
 const gorda = (num, by) => {
   let resUint32 = new Uint32Array([num]);
@@ -328,181 +520,15 @@ const deca = (ls) => {
   return dca[0];
 };
 
-const maxAndArgMax = (ls) => {
-  let currMax = ls[0];
-  let currIndex = 0;
-
-  for (let i = 1; i < cPOX_PORTION_NUM; i++) {
-    if (ls[i] > currMax) {
-      currMax = ls[i];
-      currIndex = i;
-    }
-  }
-
-  return { max: currMax, argmax: currIndex };
-};
-
-const minAndArgMin = (ls) => {
-  let currMin = ls[0];
-  let currIndex = 0;
-
-  for (let i = 1; i < cPOX_PORTION_NUM; i++) {
-    if (ls[i] < currMin) {
-      currMin = ls[i];
-      currIndex = i;
-    }
-  }
-
-  return { min: currMin, argmin: currIndex };
-};
-
-const wordArrayToByteArray = (wordarray) => {
-  const wordToBye = (word) => {
-    const lower = word & cMASK_WORD_ZZFF;
-    const upper = (word & cMASK_WORD_FFZZ) >> cBYTE_WIDTH;
-
-    return { lower: lower, upper: upper };
-  };
-
-  const upperLowerA = wordToBye(wordarray[0]);
-  const upperLowerB = wordToBye(wordarray[1]);
-  const upperLowerC = wordToBye(wordarray[2]);
-  const upperLowerD = wordToBye(wordarray[3]);
-
-  const byteArray = new Uint8Array([
-    upperLowerA.lower,
-    upperLowerA.upper,
-    upperLowerB.lower,
-    upperLowerB.upper,
-    upperLowerC.lower,
-    upperLowerC.upper,
-    upperLowerD.lower,
-    upperLowerD.upper,
-  ]);
-
-  return byteArray;
-};
-
-const wordToDouble = (wordA, wordB, dArr, index) => {
-  const wArrD = new Uint32Array([wordA, wordB]);
-  dArr[index] |= wArrD[0];
-  dArr[index] |= wArrD[1] << 16;
-};
-
-const wordArrayToDoubleArray = (wArray) => {
-  let dArr = new Uint32Array(2);
-  wordToDouble(wArray[0], wArray[1], dArr, 0);
-  wordToDouble(wArray[2], wArray[3], dArr, 1);
-  return dArr;
-};
-
-const doubleArrayToQuad = (dArr) => {
-  let quad = BigInt("0x0000000000000000");
-  quad |= BigInt(dArr[0]);
-  quad |= BigInt(dArr[1]) << BigInt(32);
-  return BigInt.asUintN(64, quad);
-};
-
-const byteArrayToWordArrayAndPad = (bytearr) => {
-  let size = bytearr.length;
-  while (size % cPOX_BLOCK_NUM != 0) {
-    size += 1;
-  }
-  let u16Array = new Uint16Array(size);
-  for (let i = 0; i < size; i++) {
-    u16Array[i] = bytearr[i];
-  }
-  return u16Array;
-};
-
-const processInput = (input) => {
-  if (typeof input == "object") {
-    if (input.constructor.name == "Uint8Array") {
-      return byteArrayToWordArrayAndPad(input);
-    }
-  }
-  return null;
-};
-
-const convertBaseFromDecimal = (base, size, chars, res, dec, offset) => {
-  for (let i = size * offset + size - 1; i >= size * offset; i--) {
-    res[i] = chars[dec % base];
-    dec = iDiv(dec, base);
-  }
-};
-
-const convertWordsToSexDigest = (words) => {
-  let sex = "0".repeat(cSEX_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cSEX_BASE, cSEX_SIZE, cSEX_CHARS, sex, words[i], i);
-  }
-  return sex.join("");
-};
-
-const convertWordsToVigDigest = (words) => {
-  let vig = "0".repeat(cVIG_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cVIG_BASE, cVIG_SIZE, cVIG_CHARS, vig, words[i], i);
-  }
-  return vig.join("");
-};
-
-const convertWordsToHexDigest = (words) => {
-  let hex = "0".repeat(cHEX_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cHEX_BASE, cHEX_SIZE, cHEX_CHARS, hex, words[i], i);
-  }
-  return hex.join("");
-};
-
-const convertWordsToTetDigest = (words) => {
-  let tet = "0".repeat(cTET_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cTET_BASE, cTET_SIZE, cTET_CHARS, tet, words[i], i);
-  }
-  return tet.join("");
-};
-
-const convertWordsToDuoDigest = (words) => {
-  let duo = "0".repeat(cDUO_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cDUO_BASE, cDUO_SIZE, cDUO_CHARS, duo, words[i], i);
-  }
-  return duo.join("");
-};
-
-const convertWordsToOctDigest = (words) => {
-  let oct = "0".repeat(cOCT_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cOCT_BASE, cOCT_SIZE, cOCT_CHARS, oct, words[i], i);
-  }
-  return oct.join("");
-};
-
-const convertWordsToSenDigest = (words) => {
-  let sen = "0".repeat(cSEN_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cSEN_BASE, cSEN_SIZE, cSEN_CHARS, sen, words[i], i);
-  }
-  return sen.join("");
-};
-
-const convertWordsToBinDigest = (words) => {
-  let bin = "0".repeat(cBIN_SIZE * cPOX_PORTION_NUM).split("");
-  for (let i = 0; i < cPOX_PORTION_NUM; i++) {
-    convertBaseFromDecimal(cBIN_BASE, cBIN_SIZE, cBIN_CHARS, bin, words[i], i);
-  }
-  return bin.join("");
-};
-
 const get8BPrime = (num) => {
   const remainder = num % cPOX_8B_PRIME_NUM;
   return cPOX_8B_PRIMES[remainder];
 };
 
-const log2N = (num) => {
-  return num > 1 ? 1 + log2N(iDiv(num, 2)) : 0;
-};
+//////// .BESPOKE OPS ////////
+//--------------------------//
+//////// ALPHABET OPS ////////
+// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#part-d-alphabet-operations
 
 const poxAlpha = (tempArray) => {
   const aleph = (tempArray[0] ^ tempArray[1]) & cMASK_WORD_ZZFF;
@@ -572,6 +598,11 @@ const poxGamma = (tempArray) => {
   tempArray[gee] ^= log2N(teth) >> ((gamal % 2) + 1);
 };
 
+//////// .ALPHABET OPS ////////
+//---------------------------//
+////////   ROUND OPS   ////////
+// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#part-e-round-methods
+
 const poxRoundApplyAlphabet = (tempArray) => {
   poxAlpha(tempArray);
   poxDelta(tempArray);
@@ -615,7 +646,10 @@ const poxRound = (factorArray) => {
   poxRoundAddTempsToFacts(factorArray, tempArray);
 };
 
-const debug = (arr) => console.log(arr[0], arr[1], arr[2], arr[3]);
+////////   /ROUND OPS   ////////
+//----------------------------//
+////////   BLOCK OPS   /////////
+// https://github.com/Chubek/PoxHash/blob/master/SPEC.md#part-f-block-methods
 
 const poxApplySubportion = (factorArray, subportion, index) => {
   const tmt = tamaam(subportion);
@@ -658,6 +692,17 @@ const poxProcessBlock = (factorArray, blockArray) => {
       }
     }
   }
+};
+
+////////   /BLOCK OPS   /////////
+
+const processInput = (input) => {
+  if (typeof input == "object") {
+    if (input.constructor.name == "Uint8Array") {
+      return byteArrayToWordArrayAndPad(input);
+    }
+  }
+  return null;
 };
 
 function PoxDigest(
