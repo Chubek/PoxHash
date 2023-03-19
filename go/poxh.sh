13654c12992b1bc09f83fcef9220139b9762fe19
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-19 14:57:19 +0330

    go compile flag

--- go/poxh.sh
+++ go/poxh.sh
@@ -1,7 +1,21 @@
-mkdir -p /tmp/poxgo /tmp/poxgo/libpoxh
-cp go/libpoxh.go /tmp/poxgo/libpoxh/libpoxh.go
-cp go/runner.go /tmp/poxgo/main.go
-rm -f /tmp/poxgo/go-poxhash-runner
-rm -f /tmp/poxgo/go.mod
-cd /tmp/poxgo  && go mod init pox &> /dev/null && go mod tidy && go build -ldflags "-s -w" -o /tmp/poxgo/go-poxhash-runner main.go 
-/tmp/poxgo/go-poxhash-runner $@
+#!/bin/bash
+
+BASE_DIR="go"
+PROJ_DIR="/tmp/gopoxh"
+LIB_NAME="libpoxh"
+RUNNER_NAME="runner.go"
+LIBPOX_NAME="libpoxh.go"
+COMPILE_NAME="go-poxhash-runner"
+MOD_NAME="go.mod"
+MAIN_NAME="main.go"
+
+if [[ "$COMPILE" = "1" ]] || [[ ! -f "$PROJ_DIR/$COMPILE_NAME" ]]; then
+    mkdir -p $PROJ_DIR $PROJ_DIR/$LIB_NAME
+    cp $BASE_DIR/$RUNNER_NAME $PROJ_DIR/$MAIN_NAME
+    cp $BASE_DIR/$LIBPOX_NAME $PROJ_DIR/$LIB_NAME/$LIBPOX_NAME
+    rm -f $PROJ_DIR/$COMPILE_NAME
+    rm -f $PROJ_DIR/$MOD_NAME
+    cd $PROJ_DIR  && go mod init pox &> /dev/null && go mod tidy && go build -ldflags "-s -w" -o $PROJ_DIR/$COMPILE_NAME main.go &> /dev/null 
+fi
+
+$PROJ_DIR/$COMPILE_NAME $@
