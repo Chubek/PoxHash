dbe0857709c17b4196e8f149ac00fc7d4cb7ea55
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-03-19 16:20:19 +0330

    removed stdout pipe

--- c/poxh.sh
+++ c/poxh.sh
@@ -7,7 +7,7 @@ FILE_PATH="c/runner.c"
 if [[ "$COMPILE" = "1" ]] || [[ ! -f "$PROJ_DIR/$COMPILE_NAME" ]]; then
     rm -f $PROJ_DIR/$COMPILE_NAME
     mkdir -p $PROJ_DIR
-    gcc -O2 $FILE_PATH -o $PROJ_DIR/$COMPILE_NAME &> /dev/null
+    gcc -O2 $FILE_PATH -o $PROJ_DIR/$COMPILE_NAME
 fi
 
 $PROJ_DIR/$COMPILE_NAME $@
